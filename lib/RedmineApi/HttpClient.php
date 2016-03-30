a99b8d38407759549e1e82823015b30502d8df40
Michail Buylov <mipxtx@gmail.com>
2016-03-30 13:07:27 +0300

    fix http client

--- lib/RedmineApi/HttpClient.php
+++ lib/RedmineApi/HttpClient.php
@@ -16,7 +16,7 @@ class HttpClient
 {
     private $debug = false;
 
-    private $debugStringLength = 0;
+    private $debugStringLength = 1000;
 
     private $key;
 
@@ -33,7 +33,8 @@ class HttpClient
      * @param string $key redmine api key (http://www.redmine.org/projects/redmine/wiki/Rest_api#Authentication)
      * @param float $timeout request timeout
      */
-    public function __construct($server_url, $key, $timeout = 0.1) {
+    public function __construct($server_url, $key, $timeout = 0.1)
+    {
         $this->server_url = $server_url;
         $this->key = $key;
         $this->timeout = $timeout;
@@ -46,47 +47,52 @@ class HttpClient
      * @return mixed
      * @throws Exception
      */
-    public function request($method, $requestUrl, array $data = []) {
-        return $this->getResponse(
-            $this->sendRequest(
-                $this->getRawRequest($method, $requestUrl, $data)
-            )
-        );
+    public function request($method, $requestUrl, array $data = [])
+    {
+
+        $request = $this->getRawRequest($method, $requestUrl, $data);
+        $h = $this->sendRequest($request);
+
+        return $this->getResponse($h, $request);
     }
 
-    public function enableDebug($length = 1024) {
+    public function enableDebug($length = 1024)
+    {
         $this->debug = true;
         $this->debugStringLength = $length;
     }
 
-    public function log($str) {
+    public function log($str)
+    {
         if ($this->debug) {
 
-            if(php_sapi_name() != "cli"){
-                echo "<pre>";
+            if (php_sapi_name() != "cli") {
+                //echo "<pre>";
             }
             echo (
-                strlen($str) > $this->debugStringLength
-                    ? (mb_strcut($str, 0, $this->debugStringLength) . "...")
-                    : $str
-                ) . "\n";
-            if(php_sapi_name() != "cli"){
-                echo "</pre>";
+            strlen($str) > $this->debugStringLength
+                ? (mb_strcut($str, 0, $this->debugStringLength) . "...")
+                : $str
+            );
+            //echo "\n";
+            if (php_sapi_name() != "cli") {
+                //echo "</pre>";
             }
         }
     }
 
-    public function getRawRequest($method, $requestUrl, array $data = []) {
+    public function getRawRequest($method, $requestUrl, array $data = [])
+    {
 
         $serverUrl = parse_url($this->server_url);
         $host = $serverUrl["host"];
 
-        if(in_array($method, ["GET", "DELETE"]) && $data){
+        if (in_array($method, ["GET", "DELETE"]) && $data) {
             $params = [];
-            foreach($data as $key => $value){
+            foreach ($data as $key => $value) {
                 $params[] = urlencode($key) . "=" . urlencode($value);
             }
-            $requestUrl .= "?" . implode("&",$params);
+            $requestUrl .= "?" . implode("&", $params);
 
         }
 
@@ -107,7 +113,8 @@ class HttpClient
         return $str;
     }
 
-    public function sendRequest($request) {
+    public function sendRequest($request)
+    {
         $scheme = "tcp";
         $port = "80";
         $serverUrl = parse_url($this->server_url);
@@ -132,14 +139,15 @@ class HttpClient
 
         $timeoutSec = floor($this->timeout);
         $timeoutMsec = $this->timeout * 1000000;
-        stream_set_timeout($h,$timeoutSec, $timeoutMsec);
+        stream_set_timeout($h, $timeoutSec, $timeoutMsec);
         fwrite($h, $request);
         $this->log($request);
 
         return $h;
     }
 
-    public function readLine($h) {
+    public function readLine($h)
+    {
         $line = fgets($h);
         if ($this->debug) {
             $this->rawResponse .= $line;
@@ -148,10 +156,11 @@ class HttpClient
         return trim($line);
     }
 
-    public function read($h, $length) {
+    public function read($h, $length)
+    {
         $toRead = $length;
         $out = "";
-        while ($toRead > 0){
+        while ($toRead > 0) {
             $blockLength = ($toRead > self::READ_BLOCK_SIZE) ? self::READ_BLOCK_SIZE : $toRead;
             $block = fread($h, $blockLength);
             $toRead -= $blockLength;
@@ -163,25 +172,42 @@ class HttpClient
         return $out;
     }
 
-    public function getResponse($h) {
+    public function getResponse($h, $request)
+    {
+        $lines = [];
+
         $headers = [];
         $line = $this->readLine($h);
+        $lines[]= $line;
         do {
             $headers[] = $line;
             $line = $this->readLine($h);
+            $lines[] = $line;
         } while ($line);
-        $length = $this->getBlockLenght($h);
-        $body = "";
-        do {
-            $block = $this->read($h, $length);
-            $body .= $block;
-            $this->readLine($h);
-            $length = $this->getBlockLenght($h);
 
-        } while ($length > 0);
+        $length = 0;
+        foreach($headers as $header){
+            if(!strpos($header, ":")){
+                continue;
+            }
+            list($name, $value) = explode(":", $header);
+            if(trim($name) == 'Content-Length'){
+                $length = $value;
+                break;
+            }
+        }
+
+        $body = $this->read($h, $length);
 
         $this->log($this->rawResponse);
         $this->rawResponse = "";
+
+        if (!$body && $length > 0) {
+            throw new Exception(
+                "bad responce: '" . implode("\n", $lines)
+                . "' on request:\n " . var_export($request, 1)
+            );
+        }
 
         $result = json_decode($body, 1);
         if (isset($result["error"])) {
@@ -191,13 +217,4 @@ class HttpClient
         return $result;
     }
 
-    private function getBlockLenght($h){
-        $delimiter = $this->readLine($h);
-        $length = 0;
-        for ($i = 0; $i < strlen($delimiter); $i++) {
-            $char = $delimiter[$i];
-            $length = ($length << 4) + (($char >= '0' && $char <= '9') ? (int)$char : (ord($char) - ord('a') + 10));
-        }
-        return $length;
-    }
 }
