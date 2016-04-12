c118e5a903180523ff202c2de344706663ae87b5
Bazilio <x.bazilio@gmail.com>
2016-04-12 18:26:44 +0300

    rewrited http client to curl

--- lib/RedmineApi/HttpClient.php
+++ lib/RedmineApi/HttpClient.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * @author: mix
- * @date: 01.05.14
- */
 namespace RedmineApi;
 
 /**
@@ -23,10 +19,6 @@ class HttpClient
     private $server_url;
 
     private $timeout;
-
-    const READ_BLOCK_SIZE = 4096;
-
-    private $rawResponse = "";
 
     /**
      * @param string $server_url ie https://redmine.example.com
@@ -49,11 +41,11 @@ class HttpClient
      */
     public function request($method, $requestUrl, array $data = [])
     {
+        $requestUrl = $this->getRequestUrl($method, $requestUrl, $data);
 
-        $request = $this->getRawRequest($method, $requestUrl, $data);
-        $h = $this->sendRequest($request);
+        $ch = $this->prepareCurl($method, $requestUrl, $data);
 
-        return $this->getResponse($h, $request);
+        return $this->getResponse($ch);
     }
 
     public function enableDebug($length = 1024)
@@ -81,140 +73,64 @@ class HttpClient
         }
     }
 
-    public function getRawRequest($method, $requestUrl, array $data = [])
+    public function getRequestUrl($method, $requestUrl, array $data = [])
     {
-
-        $serverUrl = parse_url($this->server_url);
-        $host = $serverUrl["host"];
-
-        if (in_array($method, ["GET", "DELETE"]) && $data) {
+        if ($method == 'GET' && $data) {
             $params = [];
             foreach ($data as $key => $value) {
-                $params[] = urlencode($key) . "=" . urlencode($value);
+                $params[] = urlencode($key) . '=' . urlencode($value);
             }
-            $requestUrl .= "?" . implode("&", $params);
-
+            $requestUrl .= '?' . implode('&', $params);
         }
 
-        $str = "{$method} {$requestUrl} HTTP/1.1\r\n" .
-            "Host: {$host}\r\n" .
-            "Connection: close\r\n" .
-            "X-Redmine-API-Key: {$this->key}\r\n";
-
-        if (in_array($method, ["POST", "PUT"])) {
-            $data = (json_encode($data));
-            $str .= "Content-Type: application/json\r\n";
-            $str .= "Content-Length: " . strlen($data) . "\r\n\r\n";
-            $str .= $data . "\r\n";
-        }
-
-        $str .= "\r\n";
-
-        return $str;
+        return $this->server_url . $requestUrl;
     }
 
-    public function sendRequest($request)
+    public function prepareCurl($method, $requestUrl, array $data = [])
     {
-        $scheme = "tcp";
-        $port = "80";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $requestUrl);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT_MS, $this->timeout * 1000000);
+        $this->log("connecting to $requestUrl");
+
+        if ($method != 'GET') {
+            $data = json_encode($data);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+            $this->log($data);
+        }
+
+        if ($method == 'POST') {
+            curl_setopt($ch, CURLOPT_POST, 1);
+        } else if ($method == 'PUT' || $method == 'DELETE') {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        }
+
         $serverUrl = parse_url($this->server_url);
-        $host = $serverUrl["host"];
-        if (isset($serverUrl["port"])) {
-            $port = $serverUrl["port"];
-            if ($serverUrl["scheme"] == "https") {
-                $scheme = "ssl";
-            } else {
-                $scheme = "tcp";
-            }
-        } else {
-            if ($serverUrl["scheme"] == "https") {
-                $port = "443";
-                $scheme = "ssl";
-            }
+        if ($serverUrl['scheme'] == 'https') {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
         }
 
-        $uri = "$scheme://$host:$port";
-        $this->log("connecting to $uri");
-        $h = stream_socket_client($uri, $errno, $errstr, $this->timeout);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'X-Redmine-API-Key: ' . $this->key
+        ]);
 
-        $timeoutSec = floor($this->timeout);
-        $timeoutMsec = $this->timeout * 1000000;
-        stream_set_timeout($h, $timeoutSec, $timeoutMsec);
-        fwrite($h, $request);
-        $this->log($request);
-
-        return $h;
+        return $ch;
     }
 
-    public function readLine($h)
+    public function getResponse($ch)
     {
-        $line = fgets($h);
-        if ($this->debug) {
-            $this->rawResponse .= $line;
-        }
+        $body = curl_exec($ch);
+        curl_close($ch);
 
-        return trim($line);
-    }
-
-    public function read($h, $length)
-    {
-        $toRead = $length;
-        $out = "";
-        while ($toRead > 0) {
-            $blockLength = ($toRead > self::READ_BLOCK_SIZE) ? self::READ_BLOCK_SIZE : $toRead;
-            $block = fread($h, $blockLength);
-            $toRead -= $blockLength;
-            if ($this->debug) {
-                $this->rawResponse .= $block;
-            }
-            $out .= $block;
-        }
-        return $out;
-    }
-
-    public function getResponse($h, $request)
-    {
-        $lines = [];
-
-        $headers = [];
-        $line = $this->readLine($h);
-        $lines[]= $line;
-        do {
-            $headers[] = $line;
-            $line = $this->readLine($h);
-            $lines[] = $line;
-        } while ($line);
-
-        $length = 0;
-        foreach($headers as $header){
-            if(!strpos($header, ":")){
-                continue;
-            }
-            list($name, $value) = explode(":", $header);
-            if(trim($name) == 'Content-Length'){
-                $length = $value;
-                break;
-            }
-        }
-
-        $body = $this->read($h, $length);
-
-        $this->log($this->rawResponse);
-        $this->rawResponse = "";
-
-        if (!$body && $length > 0) {
-            throw new Exception(
-                "bad responce: '" . implode("\n", $lines)
-                . "' on request:\n " . var_export($request, 1)
-            );
-        }
+        $this->log($body);
 
         $result = json_decode($body, 1);
-        if (isset($result["error"])) {
-            throw new Exception($result["error"]);
+        if (isset($result['error'])) {
+            throw new Exception($result['error']);
         }
 
         return $result;
     }
-
 }
